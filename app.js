c1d4e67bd9cb76a20ea5994392896044e214ff0e
Jef Ogues <jef.hotmail_@hotmail.com>
2022-05-31 17:14:07 -0400

    setting up for deployment

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
+var helmet = require('helmet');
+
+
 
 var app = express();
 
@@ -25,6 +29,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+
+app.use(compression()); //Compress all routes
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
